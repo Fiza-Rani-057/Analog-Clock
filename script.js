979653cb684dd725f08c848c09c630122be54f3c
Fiza-Rani-057 <fahadsaeed057@gmail.com>
2026-08-18 22:21:40 +0500

    Analog Clock

---
+++ script.js
@@ -0,0 +1,9 @@
+ const hour = document.getElementById('hour');
+ const minute = document.getElementById('minute');
+ const second = document.getElementById('second');
+
+ const digital = document.getElementById('digital');
+ const date = document.getElementById('date');
+ const days = ["MON" , "TUE" , "WED" , "THU" , "FRI" , "SAT" , "SUN"];
+const months =["JAN", "FEB", "MAR" , "APR", "MAY" , "JUN" , "JUL" , "AUG",
+    "SEP" , "OCT" , "NOV" , "DEC"];

514bfad63a3e8a95c3250ba986eee5ae96f6fe55
Fiza-Rani-057 <fahadsaeed057@gmail.com>
2026-08-18 22:56:20 +0500

    Analog Clock

--- script.js
+++ script.js
@@ -4,17 +4,27 @@ const second = document.getElementById('second');
 
 const digital = document.getElementById('digital');
 const date = document.getElementById('date');
-const days = ["MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"];
+const days = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
 const months = ["JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG",
     "SEP", "OCT", "NOV", "DEC"];
 
-function clock(){
+function clock() {
     const now = new Date();
     let h = now.getHours();
     let m = now.getMinutes();
     let s = now.getSeconds();
-    
-    hour.style.transform = `rotate(${h*30+m/20}deg)`;
-    minute.style.transform = `rotate(${m*6+s*0.1}deg)`;
-    second.style.transform = `rotate(${s*6}deg)`;
+
+    hour.style.transform = `rotate(${(h % 12) * 30 + m * 0.5}deg)`;
+    minute.style.transform = `rotate(${m * 6 + s * 0.1}deg)`;
+    second.style.transform = `rotate(${s * 6}deg)`;
+
+    digital.innerHTML = now.toLocaleTimeString([], {
+        hour: '2-digit',
+        minute: `2-digit`
+    });
+
+    date.innerHTML = `${days[now.getDay()]} ,${months[now.getMonth()]} , 
+  ${now.getDate()}`;
 }
+setInterval(clock, 1000);
+clock();
